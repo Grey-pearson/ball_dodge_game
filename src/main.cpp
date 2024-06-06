076c1db221f25eb7bd78ca0d74a40b1fb220607f
grey <greytpearson@gmail.com>
2024-06-06 10:21:17 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include "sort.h"
 
+class Screen()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 400), "SFML works!");
+    // sf::CircleShape shape(100.f);
+    // shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        // window.draw(shape);
+        window.display();
+    }
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1000, 400), "SFML works!");
