d49e09b7fac9918518397f1cba4b24fd3fd940b0
grey <greytpearson@gmail.com>
2024-06-06 10:21:23 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -24,23 +24,6 @@ class Screen()
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 400), "SFML works!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        // window.draw(shape);
-        window.display();
-    }
 
     return 0;
 }
