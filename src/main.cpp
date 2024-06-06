229856f7ebeab954fbd8d853b229f28650087e4d
grey <greytpearson@gmail.com>
2024-06-06 10:26:04 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include "sort.h"
 
+Sort sort;
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 400), "SFML works!");
-    // sf::CircleShape shape(100.f);
-    // shape.setFillColor(sf::Color::Green);
+    // sf::RenderWindow window(sf::VideoMode(1000, 400), "SFML works!");
+    // // sf::CircleShape shape(100.f);
+    // // shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    // while (window.isOpen())
+    // {
+    //     sf::Event event;
+    //     while (window.pollEvent(event))
+    //     {
+    //         if (event.type == sf::Event::Closed)
+    //             window.close();
+    //     }
 
-        window.clear();
-        // window.draw(shape);
-        window.display();
-    }
+    //     window.clear();
+    //     // window.draw(shape);
+    //     window.display();
+    // }
+
+    cout << "how many pillars do you want sorted?";
+    cin >> sort.num_of_pillars >> endl;
 
     return 0;
 }
